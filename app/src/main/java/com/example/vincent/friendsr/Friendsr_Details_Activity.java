cc5a1e9bb7a11b7cea1eb926634c5f5d227cf5cb
Vincent Erich <vincent.erich@live.nl>
2015-09-16 21:28:19 +0200

    Version 1.0 of the application.

--- app/src/main/java/com/example/vincent/friendsr/Friendsr_Details_Activity.java
+++ app/src/main/java/com/example/vincent/friendsr/Friendsr_Details_Activity.java
@@ -1,6 +1,7 @@
 package com.example.vincent.friendsr;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,12 +16,10 @@ public class Friendsr_Details_Activity extends AppCompatActivity {
     private RatingBar ratingBar;
     private ImageView detailedPicture;
     private TextView detailedInformation;
-
     private String friendName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("onCreate in Friendsr_Details_Activity...");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.friendsr_details_activity);
 
@@ -28,33 +27,16 @@ public class Friendsr_Details_Activity extends AppCompatActivity {
         detailedPicture = (ImageView) findViewById(R.id.picture);
         detailedInformation = (TextView) findViewById(R.id.details);
 
-        Intent activityThatCalled = getIntent();
-        friendName = activityThatCalled.getExtras().getString("name");
-        String friendDetails = activityThatCalled.getExtras().getString("details");
+        Bundle extras = getIntent().getExtras();
+        friendName = extras.getString(Friendsr_Main_Activity.nameKey);
+        String friendDetails = extras.getString(Friendsr_Main_Activity.detailsKey);
+        float currentFriendRating = extras.getFloat(Friendsr_Main_Activity.ratingKey);
 
-        showPictureAndDetails(friendDetails);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.friendsr_details_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (currentFriendRating != Friendsr_Main_Activity.initialRating){
+            ratingBar.setRating(currentFriendRating);
         }
 
-        return super.onOptionsItemSelected(item);
+        showPictureAndDetails(friendDetails);
     }
 
     public void showPictureAndDetails(String details) {
@@ -81,12 +63,48 @@ public class Friendsr_Details_Activity extends AppCompatActivity {
         detailedInformation.setText(details);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.friendsr_details_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     public void onRateButtonClick(View view) {
         float rating = ratingBar.getRating();
         Intent sendRating = new Intent();
-        sendRating.putExtra("rating", rating);
-        sendRating.putExtra("name", friendName);
+        sendRating.putExtra(Friendsr_Main_Activity.ratingKey, rating);
+        sendRating.putExtra(Friendsr_Main_Activity.nameKey, friendName);
         setResult(RESULT_OK, sendRating);
         finish();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        float rating = ratingBar.getRating();
+        outState.putFloat(Friendsr_Main_Activity.ratingKey, rating);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle inState) {
+        super.onRestoreInstanceState(inState);
+        float rating = inState.getFloat(Friendsr_Main_Activity.ratingKey);
+        ratingBar.setRating(rating);
     }
 }
--- app/src/main/java/com/example/vincent/friendsr/Friendsr_Main_Activity.java
+++ app/src/main/java/com/example/vincent/friendsr/Friendsr_Main_Activity.java
@@ -1,6 +1,9 @@
 package com.example.vincent.friendsr;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,14 +15,26 @@ import android.widget.Toast;
 public class Friendsr_Main_Activity extends AppCompatActivity {
 
     private TextView textViewChandler, textViewJoey, textViewMonica, textViewPhoebe, textViewRachel, textViewRoss;
-    private String[] friendNames, friendDetails; //friendFullNames
+    private String[] friendNames, friendDetails;
     private float ratingChandler, ratingJoey, ratingMonica, ratingPhoebe, ratingRachel, ratingRoss;
+    private SharedPreferences prefs;
 
-    final int result = 1;
+    private final String ratingChandlerKey = "ratingChandlerKey";
+    private final String ratingJoeyKey = "ratingJoeyKey";
+    private final String ratingMonicaKey = "ratingMonicaKey";
+    private final String ratingPhoebeKey = "ratingPhoebeKey";
+    private final String ratingRachelKey = "ratingRachelKey";
+    private final String ratingRossKey = "ratingRossKey";
+
+    public static final float initialRating = (float) -1;
+    public static final float ratingErrorSwitchingActivities = (float) -100;
+    public static final String prefsName = "myPREFERENCES";
+    public static final String nameKey = "nameKey";
+    public static final String detailsKey = "detailsKey";
+    public static final String ratingKey = "ratingKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("onCreate in Friendsr_Main_Activity...");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.friendsr_main_activity);
 
@@ -31,11 +46,15 @@ public class Friendsr_Main_Activity extends AppCompatActivity {
         textViewRoss = (TextView) findViewById(R.id.text_view_ross);
 
         friendNames = getResources().getStringArray(R.array.friend_names);
-        //friendFullNames = getResources().getStringArray(R.array.friend_full_names);
         friendDetails = getResources().getStringArray(R.array.friend_details);
 
-        setFriendNames();
+        prefs = getSharedPreferences(prefsName, Context.MODE_PRIVATE);
+
+        getPreferences();
+        showFriendNamesAndRatings();
     }
+
+    //-----------------------------------------------------------------------------
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,6 +62,8 @@ public class Friendsr_Main_Activity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.friendsr_main_menu, menu);
         return true;
     }
+
+    //-----------------------------------------------------------------------------
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -59,83 +80,198 @@ public class Friendsr_Main_Activity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void setFriendNames() {
-        textViewChandler.setText(friendNames[0]);
-        textViewJoey.setText(friendNames[1]);
-        textViewMonica.setText(friendNames[2]);
-        textViewPhoebe.setText(friendNames[3]);
-        textViewRachel.setText(friendNames[4]);
-        textViewRoss.setText(friendNames[5]);
+    //-----------------------------------------------------------------------------
+
+    private void getPreferences() {
+        ratingChandler = prefs.getFloat(ratingChandlerKey, initialRating);
+        ratingJoey = prefs.getFloat(ratingJoeyKey, initialRating);
+        ratingMonica = prefs.getFloat(ratingMonicaKey, initialRating);
+        ratingPhoebe = prefs.getFloat(ratingPhoebeKey, initialRating);
+        ratingRachel = prefs.getFloat(ratingRachelKey, initialRating);
+        ratingRoss = prefs.getFloat(ratingRossKey, initialRating);
     }
 
+    //-----------------------------------------------------------------------------
+
+    private void showFriendNamesAndRatings() {
+        if (ratingChandler != initialRating) {
+            textViewChandler.setText(friendNames[0] + " (" + ratingChandler + ")");
+        } else {
+            textViewChandler.setText(friendNames[0] + " " + getString(R.string.text_not_rated));
+        }
+
+        if (ratingJoey != initialRating) {
+            textViewJoey.setText(friendNames[1] + " (" + ratingJoey + ")");
+        } else {
+            textViewJoey.setText(friendNames[1] + " " + getString(R.string.text_not_rated));
+        }
+
+        if (ratingMonica != initialRating) {
+            textViewMonica.setText(friendNames[2] + " " + getString(R.string.text_not_rated));
+        } else {
+            textViewMonica.setText(friendNames[2] + " " + getString(R.string.text_not_rated));
+        }
+
+        if (ratingPhoebe != initialRating) {
+            textViewPhoebe.setText(friendNames[3] + " (" + ratingPhoebe + ")");
+        } else {
+            textViewPhoebe.setText(friendNames[3] + " " + getString(R.string.text_not_rated));
+        }
+
+        if (ratingRachel != initialRating) {
+            textViewRachel.setText(friendNames[4] + " (" + ratingRachel + ")");
+        } else {
+            textViewRachel.setText(friendNames[4] + " " + getString(R.string.text_not_rated));
+        }
+
+        if (ratingRoss != initialRating) {
+            textViewRoss.setText(friendNames[5] + " (" + ratingRoss + ")");
+        } else {
+            textViewRoss.setText(friendNames[5] + " " + getString(R.string.text_not_rated));
+        }
+    }
+
+    //-----------------------------------------------------------------------------
+
     public void onClickChandler(View view) {
-        showDetails("Chandler", friendDetails[0]);
+        showDetails(friendNames[0], friendDetails[0], ratingChandler);
     }
 
     public void onClickJoey(View view) {
-        showDetails("Joey", friendDetails[1]);
+        showDetails(friendNames[1], friendDetails[1], ratingJoey);
     }
 
     public void onClickMonica(View view) {
-        showDetails("Monica", friendDetails[2]);
+        showDetails(friendNames[2], friendDetails[2], ratingMonica);
     }
 
     public void onClickPhoebe(View view) {
-        showDetails("Phoebe", friendDetails[3]);
+        showDetails(friendNames[3], friendDetails[3], ratingPhoebe);
     }
 
     public void onClickRachel(View view) {
-        showDetails("Rachel", friendDetails[4]);
+        showDetails(friendNames[4], friendDetails[4], ratingRachel);
     }
 
     public void onClickRoss(View view) {
-        showDetails("Ross", friendDetails[5]);
+        showDetails(friendNames[5], friendDetails[5], ratingRoss);
     }
 
-    private void showDetails(String name, String details) {
+    //-----------------------------------------------------------------------------
+
+    private void showDetails(String name, String details, float currentRating) {
         Intent goToDetails = new Intent(this, Friendsr_Details_Activity.class);
-        goToDetails.putExtra("name", name);
-        goToDetails.putExtra("details", details);
+        goToDetails.putExtra(nameKey, name);
+        goToDetails.putExtra(detailsKey, details);
+        goToDetails.putExtra(ratingKey, currentRating);
+        int result = 1;
         startActivityForResult(goToDetails, result);
     }
+
+    //-----------------------------------------------------------------------------
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        float rating = data.getFloatExtra("rating", (float) -100);
-        Toast.makeText(this, getString(R.string.text_rating_toast) + rating, Toast.LENGTH_SHORT).show();
 
-        String name = data.getStringExtra("name");
-        setAndWriteRating(name, rating);
+        if (data == null) {
+            Toast.makeText(this, R.string.text_no_rating_toast, Toast.LENGTH_SHORT).show();
+        } else {
+            float rating = data.getFloatExtra(ratingKey, ratingErrorSwitchingActivities);
+
+            if(rating == ratingErrorSwitchingActivities) {
+                Toast.makeText(this, R.string.text_error_switching_activities, Toast.LENGTH_SHORT);
+            } else {
+                Toast.makeText(this, getString(R.string.text_rating_toast) + " " + rating, Toast.LENGTH_SHORT).show();
+                String name = data.getStringExtra(nameKey);
+                setFriendRating(name, rating);
+                saveRatingToPreferences(name, rating);
+                showFriendNamesAndRatings();
+            }
+        }
     }
 
-    private void setAndWriteRating(String name, float rating) {
-        switch(name){
+    //-----------------------------------------------------------------------------
+
+    private void setFriendRating(String name, float rating) {
+        switch(name) {
             case "Chandler":
                 ratingChandler = rating;
-                textViewChandler.setText(name + " (" + ratingChandler + ")");
                 break;
             case "Joey":
                 ratingJoey = rating;
-                textViewJoey.setText(name + " (" + ratingJoey + ")");
                 break;
             case "Monica":
                 ratingMonica = rating;
-                textViewMonica.setText(name + " (" + ratingMonica + ")");
                 break;
             case "Phoebe":
                 ratingPhoebe = rating;
-                textViewPhoebe.setText(name + " (" + ratingPhoebe + ")");
                 break;
             case "Rachel":
                 ratingRachel = rating;
-                textViewRachel.setText(name + " (" + ratingRachel + ")");
                 break;
             case "Ross":
                 ratingRoss = rating;
-                textViewRoss.setText(name + " (" + ratingRoss + ")");
                 break;
         }
     }
+
+    //-----------------------------------------------------------------------------
+
+    private void saveRatingToPreferences(String name, float rating) {
+        SharedPreferences.Editor editor = prefs.edit();
+        switch (name) {
+            case "Chandler":
+                editor.putFloat(ratingChandlerKey, rating);
+                break;
+            case "Joey":
+                editor.putFloat(ratingJoeyKey, rating);
+                break;
+            case "Monica":
+                editor.putFloat(ratingMonicaKey, rating);
+                break;
+            case "Phoebe":
+                editor.putFloat(ratingPhoebeKey, rating);
+                break;
+            case "Rachel":
+                editor.putFloat(ratingRachelKey, rating);
+                break;
+            case "Ross":
+                editor.putFloat(ratingRossKey, rating);
+                break;
+        }
+        editor.apply();
+    }
+
+    //-----------------------------------------------------------------------------
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putFloat(ratingChandlerKey, ratingChandler);
+        outState.putFloat(ratingJoeyKey, ratingJoey);
+        outState.putFloat(ratingMonicaKey, ratingMonica);
+        outState.putFloat(ratingPhoebeKey, ratingPhoebe);
+        outState.putFloat(ratingRachelKey, ratingRachel);
+        outState.putFloat(ratingRossKey, ratingRoss);
+    }
+
+    //-----------------------------------------------------------------------------
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle inState) {
+        super.onRestoreInstanceState(inState);
+
+        ratingChandler = inState.getFloat(ratingChandlerKey);
+        ratingJoey = inState.getFloat(ratingJoeyKey);
+        ratingMonica = inState.getFloat(ratingMonicaKey);
+        ratingPhoebe = inState.getFloat(ratingPhoebeKey);
+        ratingRachel = inState.getFloat(ratingRachelKey);
+        ratingRoss = inState.getFloat(ratingRossKey);
+        showFriendNamesAndRatings();
+    }
+
+    //-----------------------------------------------------------------------------
 
 }
